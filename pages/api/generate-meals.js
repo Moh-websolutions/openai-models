35dd0e686b317fa43376e11cad1506f215a8b4d6
Moh-websolutions <mahmoud@websolutions.ca>
2023-02-24 12:02:40 -0400

    change temperature

--- pages/api/generate-meals.js
+++ pages/api/generate-meals.js
@@ -15,7 +15,7 @@ export default async function (req, res) {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: prompt,
-    temperature: 0.6,
+    temperature: 1,
     max_tokens: 2048,
   });
 
@@ -23,6 +23,12 @@ export default async function (req, res) {
 }
 
 function generatePrompt(gender, age, weight, fatLose, foods) {
-  return `Write four meal plans for each day of the week to help lose fat for a ${age}-year-old ${gender} with a weight of ${weight} lbs who is into ${foods}
+  return `Write four meal plans for Monday-Tuesday then Wednesday-Thursday and Friday-Sunday: to help lose fat for a man or woman who is into ${foods}
+
+   
+  1. Breakfast: Omelet with 2 eggs, 1/4 cup diced onion, 1/4 cup diced bell pepper, 1/4 cup diced mushrooms, 1/4 cup diced chicken, and 2 tablespoons shredded cheese.
+  2. Lunch: Chicken and vegetable stir-fry with 1 cup cooked brown rice.
+  3. Snack: Greek yogurt with 1/4 cup diced strawberries and 1/4 cup granola.
+  4. Dinner: Baked chicken breast with 1/2 cup cooked quinoa and roasted vegetables.
   `;
 }
